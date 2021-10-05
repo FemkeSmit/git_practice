6de2bed712601e0f42df526c4fc4abfdccf791a1
Bastien <bastien-ni@hotmail.be>
2021-10-05 13:22:14 +0200

    Modify autoscaling to get mean and sd

--- autoscaling.R
+++ autoscaling.R
@@ -1,8 +1,14 @@
 autoscaling <- function(dat){
   m <- colMeans(dat)
   s <- apply(dat,2,sd)
+  res <- vector('list',3)
+  
   dat2 <- sweep(dat,2,m,'-')
   dat2 <- sweep(dat2,2,s,'/')
-  return(dat2)
+  res[[1]] <- dat2
+  res[[2]] <- m
+  res[[3]] <- s
+  names(res) <- c('matrix','mean','sd')
+  return(res)
 }
 
