0fe0ef0915c74e99c4f3249c52fe89cd275d2f59
rbradford2 <rbradford2@gmail.com>
2021-10-07 16:35:43 +0200

    no anger, only peaceful updates

--- Mean_center.R
+++ Mean_center.R
@@ -1,14 +1,15 @@
-# small dataset
-set.seed(212)
-Data = matrix(rnorm(15), 5, 3)
+# # small dataset
+# set.seed(212)
+# testarooni = matrix(rnorm(15), 5, 3)
 
-Data
+
 
 # center with 'colMeans()'
-center_colmeans <- function(x) {
-  xcenter = colMeans(x)
-  x - rep(xcenter, rep.int(nrow(x), ncol(x)))
+center_sweep <- function(x, row.w = rep(1, nrow(x))/nrow(x)) {
+  get_average <- function(v) sum(v * row.w/sum(row.w))
+  average <- apply(x, 2, get_average)
+  sweep(x, 2, average)
 }
 
 # apply it
-center_colmeans(Data)
+#center_sweep(testarooni)
